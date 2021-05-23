28fdc89837aab5a9b20a32f8c4f65a2b412a4f1d
Hitesh Patel <hpatel.hp2001@gmail.com>
2021-05-23 17:27:09 +0530

    database created success

--- app/src/main/java/com/example/notes/database/NotesDatabase.kt
+++ app/src/main/java/com/example/notes/database/NotesDatabase.kt
@@ -1,9 +1,31 @@
 package com.example.notes.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Notes::class], version = 1, exportSchema = false)
 abstract class NotesDatabase : RoomDatabase() {
 
+    abstract fun getNotesDao(): NotesDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: NotesDatabase? = null
+
+        fun getDatabase(context: Context): NotesDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NotesDatabase::class.java,
+                    "notes_database"
+                ).build()
+                INSTANCE = instance
+                //return
+                instance
+            }
+        }
+    }
 }
